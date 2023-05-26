ab8f10869d232c26f40ad963ed24e24490018033
Sammyferreira1 <sammyfe2021@gmail.com>
2023-05-26 19:52:40 -0300

    left part

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,57 +3,53 @@ import Item from '@/components/Item'
 import CardBot from './CardBot'
 import CardTop from './CardTop'
 
-const items = [
-  {
-    name: 'Monitor Gamer Curvo 49 DQHD, 240Hz, 1ms, HDMI e DisplayPort, HDR 1000, FreeSync Premium, Ajuste de Altura - LC49G95TSSLXZD',
-    preço: 8599.9,
-    id: 1,
-    img: '/monitor.jpg',
-  },
-  {
-    name: 'Monitor Gamer Curvo 49 DQHD, 240Hz, 1ms, HDMI e DisplayPort, HDR 1000, FreeSync Premium, Ajuste de Altura - LC49G95TSSLXZD',
-    preço: 8599.9,
-    id: 1,
-    img: '/monitor.jpg',
-  },
-  {
-    name: 'Cadeira Gamer  RGB - Preta com Iluminação (Led)',
-    preço: 959.9,
-    id: 2,
-    img: '/cadeira.jpg',
-  },
-  {
-    name: 'Teclado Gamer Mecânico Low Profile RGB AW510K 580',
-    preço: 1002,
-    id: 3,
-    img: '/teclado.jpg',
-  },
-  {
-    name: 'Headset Gamer RGB Preto',
-    preço: 99.9,
-    id: 4,
-    img: '/headset.jpg',
-  },
-  {
-    name: 'Patinho De Borracha Para Banho',
-    preço: 19.9,
-    id: 5,
-    img: '/paatinho.jpg',
-  },
-]
-
 const Cart = () => {
-  let final: any = 0
+  const items = [
+    {
+      name: 'Monitor Gamer Curvo 49 DQHD, 240Hz, 1ms, HDMI e DisplayPort, HDR 1000, FreeSync Premium, Ajuste de Altura - LC49G95TSSLXZD',
+      preço: 8599.9,
+      id: 1,
+      img: '/monitor.jpg',
+    },
+    {
+      name: 'Cadeira Gamer  RGB - Preta com Iluminação (Led)',
+      preço: 959.9,
+      id: 2,
+      img: '/cadeira.jpg',
+    },
+    {
+      name: 'Teclado Gamer Mecânico Low Profile RGB AW510K 580',
+      preço: 1002,
+      id: 3,
+      img: '/teclado.jpg',
+    },
+    {
+      name: 'Headset Gamer RGB Preto',
+      preço: 99.9,
+      id: 4,
+      img: '/headset.jpg',
+    },
+    {
+      name: 'Patinho De Borracha Para Banho',
+      preço: 19.9,
+      id: 5,
+      img: '/paatinho.jpg',
+    },
+  ]
+  let precoFinal: any = 0
   for (let int = 0; int < items.length; int++) {
     const num = items[int].preço
-    final += num
+    precoFinal += num
   }
   return (
-    <div className="absolute right-0 h-screen w-[30%] bg-neutrals-surface">
+    <div
+      id="cart"
+      className="fixed right-0 top-0 h-[90vh] w-[30%] overflow-hidden overflow-y-scroll bg-neutrals-surface"
+    >
       {/* TOP */}
       <CardTop quantidade={items.length} />
       {/* MID */}
-      <div className=" z-0 flex flex-col gap-12 bg-neutrals-surface pb-72 pl-8 pr-8 pt-28">
+      <div className=" z-0 flex flex-col gap-12 bg-neutrals-surface pb-52 pl-8 pr-8 pt-28">
         {items.map((it) => (
           <Item
             name={it.name}
@@ -68,7 +64,7 @@ const Cart = () => {
 
       {/* BOT */}
       <CardBot
-        total={final.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
+        total={precoFinal.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
       />
     </div>
   )
